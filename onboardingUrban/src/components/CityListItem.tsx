f00f0fcddb59a142e8c62ef916cdac915c2fc901
Jakub Urban <iakub.urban@gmail.com>
2022-11-08 10:32:11 +0100

    Implement the most basic list with ugly items but that's just for now

---
+++ onboardingUrban/src/components/CityListItem.tsx
@@ -0,0 +1,54 @@
+import React, { FC } from "react";
+import { View, Text, StyleSheet } from "react-native";
+
+interface CityItemProps {
+  cityName: string;
+}
+
+export const CityItem: FC<CityItemProps> = ({ cityName }) => {
+  return (
+    <View style={styles.listItemContainer}>
+      <View style={styles.cityDescriptionContainer}>
+        <View>
+          <Text>ICON</Text>
+        </View>
+        <View style={styles.cityNameAndStatusContainer}>
+          <Text>{cityName}</Text>
+          <Text>Weather status</Text>
+        </View>
+      </View>
+      <View style={styles.temperatureLabelContainer}>
+        <Text style={styles.temperatureText}>39</Text>
+        <Text style={styles.temperatureUnitText}>°C</Text>
+      </View>
+    </View>
+  );
+};
+
+const babyBlue = "rgb(171,219,227)";
+const grey = "grey";
+const white = "rgb(255,255,255)";
+
+const styles = StyleSheet.create({
+  listItemContainer: {
+    padding: 10,
+    flex: 1,
+    flexDirection: "row",
+    borderBottomWidth: 1,
+    borderColor: grey,
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  temperatureLabelContainer: {
+    backgroundColor: babyBlue,
+    paddingVertical: 7,
+    paddingHorizontal: 20,
+    borderRadius: 20,
+    flexDirection: "row",
+    color: white,
+  },
+  cityDescriptionContainer: { flexDirection: "row", alignItems: "center" },
+  cityNameAndStatusContainer: { flexDirection: "column", marginLeft: 15 },
+  temperatureText: { color: white },
+  temperatureUnitText: { marginLeft: 5, color: white },
+});
